5faf461a07ec54b53d8351131e709b16cfabfdc6
paulsenm <paulsenm@my.lanecc.edu>
2023-10-08 13:00:08 -0700

    Winner status displaying correctly now

--- src/App.js
+++ src/App.js
@@ -1,111 +1,3 @@
-/* import React, { Component } from 'react';
-import './App.css';
-import Status from './components/Status';
-import Square from './components/Square';
-
-class App extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      appTitle: "ReacTTT",
-      appMessage: "TicTacToe app in React",
-      squares: Array(9).fill(null),
-      xIsNext: true,
-      winner: null,
-      winningLine: []
-    };
-
-    this.lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        ];
-  this.handleClick = this.handleClick.bind(this);
-}
-
-handleClick(event){
-  console.log("Event was: " + event.target.id);
-  let squares = [...this.state.squares];
-  let i = event.target.id
-  squares[i] = this.state.xIsNext ? 'X' : 'O';
-  const theWinner = this.calculateWinner(squares);
-  this.winner = theWinner.player;
-  this.winningLine = theWinner.winningLine;
-  this.xIsNext = !(this.xIsNext); 
-  this.setState({
-    squares: squares,
-    xIsNext: !this.state.xIsNext,
-    winner: theWinner.player,
-    winningLine: theWinner.winningLine
-  })
-}
-
-renderSquare(i){
-  const className = (this.state.squares[i] == null) ? "square" :
-  (this.state.winner != null && 
-  this.state.winner === this.state.squares[i]) &&
-  this.state.winningLine.includes(i) ? 
-  "square-winner" : "square-full";
-  const enabled = (this.state.winner == null && this.state.squares[i] == null) ? true : false;
-  const eventHandler = (enabled)? this.handleClick : () => {} ;
-  const value =  (this.state.squares[i] != null) ? this.state.squares[i] : "";
-  return (
-    <Square
-      className = {className}
-      value={value}
-      eventHandler={eventHandler}
-      index={i}
-      />
-  );
-}
-
-calculateWinner(squares) {
-  for (let i = 0; i < this.lines.length; i++) {
-    const [a, b, c] = this.lines[i];
-    if (squares[a] && 
-      squares[a] === squares[b] && 
-      squares[a] === squares[c]) {
-          return {player: squares[a], winningLine: this.lines[i]};
-    }
-  }
-  return {player: null, winningLine: []};
-}
-
-  render() {
-    let status;
-    if (this.state.winner) {
-      status = 'Winner: ' + this.state.winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    const output =  
-      <div>
-        <Status status = {status} />
-        <div className="board-row">
-          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
-        </div>
-      </div>
-      ;
-      return output;
-  }
-}
-
-export default App;
-
-*/
-
 import React, { useState } from 'react';
 import './App.css';
 import Status from './components/Status';
@@ -142,6 +34,21 @@ function App(){
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState({winner: null, winningLine: []});
 
+  const handleClick = (event) => {
+    console.log("Event was: " + event.target.id);
+    let localSquares = [...squares];
+    let i = parseInt(event.target.id);
+    localSquares[i] = xIsNext ? 'X' : 'O';
+    const theWinner = calculateWinner(localSquares);
+    
+    console.log("Pre setWinner: " + winner.winner || "none yet");
+    setWinner(theWinner);
+    console.log("Post setWinner: " + winner.winner || "none yet");
+
+    setSquares(localSquares);
+    setXIsNext(!xIsNext);
+  }
+
   const renderSquare = (i) => {
     const className = (squares[i] == null) ? "square" :
       (winner.winner != null && 
@@ -161,20 +68,9 @@ function App(){
     );
   }
 
-  const handleClick = (event) => {
-    console.log("Event was: " + event.target.id);
-    let localSquares = [...squares];
-    let i = event.target.id
-    localSquares[i] = xIsNext ? 'X' : 'O';
-    const theWinner = calculateWinner(squares);
-    
-    
-    setSquares(localSquares);
-    setXIsNext(!xIsNext);
-    setWinner(theWinner);
-  }
 
     let status;
+    setTimeout(1000);
     if (winner.winner) {
       status = 'Winner: ' + winner.winner;
     } else {
@@ -194,7 +90,7 @@ function App(){
           {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
         </div>
       </div>
-      );
+    );
   
 
 }
